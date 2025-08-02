a4e6dabbdd30aeae1370b1845f09623855e2bc21
lilsea7 <summersea.ae@gmail.com>
2025-08-02 09:06:00 +0700

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -11,7 +11,7 @@ import { ToastContainer } from 'react-toastify'
 
 const App = () => {
   
-  const url = "http://localhost:4000"
+  const url = "https://rabbit-cake-backend.onrender.com"
 
   return (
     <div>
