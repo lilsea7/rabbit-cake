1dfc0819ef98a3a22e060fd9721c1f73f6c26cca
lilsea7 <summersea.ae@gmail.com>
2025-08-02 11:35:31 +0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,46 +1,4 @@
-// import React, { useState } from 'react'
-// import Navbar from './components/Navbar/Navbar'
-// import { Route, Routes } from 'react-router-dom'
-// import LandingPage from './pages/LandingPage/LandingPage'
-// import Home from './pages/Home/Home'
-// import Cart from './pages/Cart/Cart'
-// import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
-// import Footer from './components/Footer/Footer'
-// import LoginPopup from './components/LoginPopup/LoginPopup'
-// import Verify from './pages/Verify/Verify'
-// import MyOrders from './pages/MyOrders/MyOrders'
-// import ProductDetail from './pages/ProductDetail/ProductDetail'
-// import Favorites from './pages/Favorites/Favorites'
-// import EditProfile from './components/EditProfile/EditProfile'
 
-// const App = () => {
-
-//   const [showLogin,setShowLogin] = useState(false)
-
-//   return (
-//     <>
-//     {showLogin?<LoginPopup setShowLogin={setShowLogin} />:<></>}
-//     <div className="app">
-//       <Navbar setShowLogin={setShowLogin}/>
-//       <Routes>
-//         <Route path='/' element={<LandingPage />} />
-//         < Route path='/home' element={<Home/>} />
-//         < Route path='/cart' element={<Cart/>} />
-//         < Route path='/order' element={<PlaceOrder/>} />
-//         <Route path='/verify' element={<Verify/>} />
-//         <Route path='/myorders' element={<MyOrders/>} />
-//         <Route path="/favorites" element={<Favorites />} /> {/* Thêm route */}
-//         <Route path="/product/:id" element={<ProductDetail />} />
-//         <Route path='/edit-profile' element={<EditProfile />}/>
-//       </Routes>
-//     </div>
-//     <Footer/>
-//     </>
-
-//   )
-// }
-
-// export default App
 
 import React, { useState } from 'react';
 import Navbar from './components/Navbar/Navbar';
